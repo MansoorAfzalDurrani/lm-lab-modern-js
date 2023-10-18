4e2b21e5d7961682ce216c2079ba0898e62962b2
MansoorAfzalDurrani <145850263+MansoorAfzalDurrani@users.noreply.github.com>
2023-10-18 16:50:00 +0100

    Feat ternaries operator code Written

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,7 +1,7 @@
 // Instructions can be found in ternaries.md
 
 export function sayHello(name) {
-  if (name === undefined) {
-    return "Hello you!";
-  } else return "Hello, " + name + "!";
-}
+   const greeting = name === undefined ? "Hello you!" : "Hello, " +name + "!";
+   console.log(greeting);
+   return greeting;
+  }
