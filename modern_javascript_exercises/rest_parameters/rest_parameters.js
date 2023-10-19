e4ecc36438e5b343139dc1eace8f0da28c9106ec
MansoorAfzalDurrani <145850263+MansoorAfzalDurrani@users.noreply.github.com>
2023-10-19 12:45:47 +0100

    Feat Function add using Rest Parameters Code Added

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,15 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
+export function add(... theAgrs) {
   // Add a loop here
-  return a + b;
+  let total =0;
+  //console.log(...theAgrs);
+  for (let arg of theAgrs ){
+    total +=arg;
+  }
+
+  return total;
 }
 
 add(1, 2, 3, 4, 5);
